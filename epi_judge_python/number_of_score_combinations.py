fd865b9b22a8e9b5e4589403b43ebe128543eacb
gopakumar <gopakumar.in@gmail.com>
2020-12-18 12:04:37 +0000

    tried array based memization

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -5,13 +5,14 @@ from test_framework import generic_test
 
 def num_combinations_for_final_score(final_score: int,
                                      individual_play_scores: List[int]) -> int:
+
     myd={}
 
     def helper (score:int, runs:List[int]) -> int:
         if((score,len(runs)) in myd ):
             return myd[(score,len(runs))]
 
-        # TODO - you fill in here..
+        # TODO - you fill in here.
         if score == 0:
             return 1
         elif score < 0:
@@ -23,9 +24,31 @@ def num_combinations_for_final_score(final_score: int,
         myd[(score,len(runs))] = ret
         return ret
 
+    '''memo = [[0 for _ in range(final_score+1)] for i in range(len(individual_play_scores)+1)]
+    def helper1 (score:int, runs:List[int]) -> int:
+        #print(score,len(runs))
+        
+        # TODO - you fill in here.
+        if score == 0:
+            return 1
+        elif score < 0:
+            return 0
+        tval = memo[len(runs)][score]
+        if tval > 0:
+            return tval
 
+        ret = 0
+        for i in range(len(runs)):
+            ind = runs[i]
+            ret += helper1(score - ind,runs[i:])
+        memo[len(runs)][score] = ret
+        #print (len(runs),score,ret)
+        return ret
+
+    result =  helper1(final_score,individual_play_scores[:])
+    #print(memo)
+   '''
     return helper(final_score,individual_play_scores[:])
-
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('number_of_score_combinations.py',
